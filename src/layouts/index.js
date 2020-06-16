446b2e8074d304603961b59129ab00f1d589eeeb
taurusdigitaldesign <taurusdigitaldesign@gmail.com>
2020-06-16 15:06:02 +0800

    fix: componentization

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import picLogo from '../.assets/images/logo.jpg';
 
-const DefaultLayout = (props) => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#">
-      <img src={picLogo} height="30" className="d-inline-block align-top" alt="logo" />
-      React-Bootstrap
-    </Navbar.Brand>
-    <Navbar.Toggle />
-    <Navbar.Collapse className="justify-content-end">
-      <Nav>
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#home">Works</Nav.Link>
-        <Nav.Link href="#link">Contact</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+const DefaultLayout = ({children}) => (
+  <div>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#">
+        <img src={picLogo} height="30" className="d-inline-block align-top" alt="logo" />
+        React-Bootstrap
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Nav>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#home">Works</Nav.Link>
+          <Nav.Link href="#link">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+
+    {children}
+  </div>
 );
 
 export default DefaultLayout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
 import { MetaData } from '../components';
-
-import picLogo from '../.assets/images/logo.jpg';
+import DefaultLayout from '../layouts';
 
 export default () => (
   <>
     <MetaData />
-    <div>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#">
-          <img src={picLogo} height="30" className="d-inline-block align-top" />
-          React-Bootstrap
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#home">Works</Nav.Link>
-            <Nav.Link href="#link">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+    <DefaultLayout>
+      <div>aaaa</div>
+    </DefaultLayout>
   </>
 );
