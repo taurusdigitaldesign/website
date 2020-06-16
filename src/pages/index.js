57b604ae9bbac4976959a82ecda6078db667c3da
taurusdigitaldesign <taurusdigitaldesign@gmail.com>
2020-06-16 17:53:05 +0800

    fix: wow.js have to import at componentdidmount, it can find window object

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
-import WOW from 'wow.js';
 import { MetaData, Section1 } from '../components';
 import DefaultLayout from '../layouts';
 
 
 export default () => {
   useEffect(() => {
-    var wow = new WOW({
+    const WOW = require('wow.js');
+    const wow = new WOW({
       boxClass: "wow",
       animateClass: "animated",
       offset: 0,
