ef1cb44d60427b1d7747bfde7b03c779f275e912
taurusdigitaldesign <taurusdigitaldesign@gmail.com>
2020-06-19 14:12:47 +0800

    fix: modify contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import { MetaData } from '../components';
 import DefaultLayout from '../layouts';
+import Style from './contact.module.scss';
 
-export default () => (
+export const query = graphql`
+  {
+    dataJson {
+      email
+    }
+  }
+`;
+
+export default ({ data }) => (
   <>
     <MetaData />
     <DefaultLayout>
       <div className="pageContain">
-        <div>contact us</div>
+        <div className={Style.box}>
+          <h2>Let's talk</h2>
+          <div>
+            Have questions about Taurus Digital Design or anything else? Send us a message and we'll
+            get back to you.
+          </div>
+          <a href={`mailto:${data.dataJson.email}`}>{data.dataJson.email}</a>
+        </div>
       </div>
     </DefaultLayout>
   </>
